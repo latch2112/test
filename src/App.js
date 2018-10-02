f0d5ae9a87fe1a72f60d41d3d32951f7b730340f
admin <admin@admins-MacBook-Pro.local>
2018-10-01 22:35:28 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+
+
+import { Paper, Typography, TextField, Button } from '@material-ui/core'
+import { List, ListItem, ListItemText } from '@material-ui/core'
+import {ListItemSecondaryAction, IconButton } from '@material-ui/core'
+import { Delete } from '@material-ui/icons'
+import { withStyles } from '@material-ui/core/styles'
+
+
+import './App.css';
+
+const styles = {
+  root: {
+    margin: 20,
+    padding: 20,
+    maxWidth: 400
+  },
+  form: {
+    display: 'flex',
+    alignItems: 'baseline',
+    justifyContent: 'space-evenly'
+  }
+    };
+
+class App extends Component {
+  
+  state = {
+    exercises: [
+      { id: 1, title: 'Bench Press' },
+      { id: 2, title: 'Deadlift' },
+      { id: 3, title: 'Squats' }
+    ],
+    title: ''
+  }
+
+
+  handleChange = ({ target: { name, value } }) =>
+    this.setState({
+      [name]: value
+    })
+
+  handleCreate = e => {
+    e.preventDefault()
+    if (this.state.title) {
+      this.setState(({ exercises, title }) => ({
+        exercises: [
+          ...exercises,
+          {
+            title,
+            id: Date.now()
+          }
+        ],
+        title: ''
+      }))
+    }
+  }
+  
+  
+  
+  handleDelete = id =>
+    this.setState(({ exercises }) => ({
+      exercises: exercises.filter(ex => ex.id !== id)
+    }))
+  
+  
+  render() {
+    const { title, exercises } = this.state
+      const { classes } = this.props
+
+    
+    return (
+      <Paper className={classes.root}>
+        <Typography variant='display1' align='center' gutterBottom>
+          Exercises
+        </Typography>
+      
+        <form onSubmit={this.handleCreate}>
+          <TextField
+            name='title'
+            label='Exercise'
+            value={title}
+            onChange={this.handleChange}
+            margin='normal'
+          />
+          <Button className={classes.button}
+            type='submit'
+            color='primary'
+            variant="contained"
+          >Create
+          </Button>
+        </form>
+        
+        <List>
+        {exercises.map(({ id, title }) =>
+          <ListItem key={id}>
+            <ListItemText primary={title} />
+            <ListItemSecondaryAction>
+              <IconButton color='primary' onClick={() => this.handleDelete(id)}>
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        )}
+        </List>
+      
+      </Paper>
+    );
+  }
+}
+
+//export default App;
+
+export default withStyles(styles)(App);
